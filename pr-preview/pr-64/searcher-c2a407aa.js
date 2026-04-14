e39e3fa3f2bf037b766804c24ce6b76fab1552f7
ch1bo <ch1bo@users.noreply.github.com>
2026-04-14 19:19:12 +0000

    Deploy preview for PR 64 🛫

--- pr-preview/pr-64/searcher-c2a407aa.js
+++ pr-preview/pr-64/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-97a749bc.js',
+                path_to_root + 'searchindex-fb9648ee.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
