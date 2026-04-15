431fa899609a04e957568656f6fe21ed4572ac43
ch1bo <ch1bo@users.noreply.github.com>
2026-04-15 20:27:53 +0000

    Deploy preview for PR 65 🛫

--- pr-preview/pr-65/searcher-c2a407aa.js
+++ pr-preview/pr-65/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e1cd8a5f.js',
+                path_to_root + 'searchindex-1b3b8593.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
