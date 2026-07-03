ca18f4220ccd155d3c122ac9f8d73df822c5dcf5
ch1bo <ch1bo@users.noreply.github.com>
2026-07-03 16:16:18 +0000

    Deploy preview for PR 67 🛫

--- pr-preview/pr-67/searcher-c2a407aa.js
+++ pr-preview/pr-67/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d06bcead.js',
+                path_to_root + 'searchindex-31bdc77b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
