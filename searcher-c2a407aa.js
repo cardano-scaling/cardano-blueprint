3019bb8fcec3eceafec21f34f451acd537c50083
jasagredo <jasataco@gmail.com>
2026-07-20 12:59:30 +0000

    Deploying to gh-pages from @ cardano-scaling/cardano-blueprint@0f0c17e1ca24b062c868d216ae50708fc19c83ab 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f4fac43f.js',
+                path_to_root + 'searchindex-ee96c1a1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
