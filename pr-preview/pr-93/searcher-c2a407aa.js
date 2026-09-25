d8a58b49d977be3cf6aa887aa424ca226dc96b98
ch1bo <ch1bo@users.noreply.github.com>
2026-09-25 11:08:43 +0000

    Deploy preview for PR 93 🛫

--- pr-preview/pr-93/searcher-c2a407aa.js
+++ pr-preview/pr-93/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a723a5dd.js',
+                path_to_root + 'searchindex-ce912898.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
